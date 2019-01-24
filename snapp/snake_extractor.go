2c5b5685d1b22562317f504ece4493dfa3b794be
Christopher Gwilliams <encima@gmail.com>
2019-01-24 14:59:26 +0100

    removed empty entries and entries with ( from snake data

--- snapp/snake_extractor.go
+++ snapp/snake_extractor.go
@@ -12,13 +12,12 @@ import (
 )
 
 type Snake struct {
-	Binomial string  `json:"name"`// index 3
-	Family string `json:"family"`//index 2
-	Genus string `json:"genus"` //index 1
+	Family   string        `json:"family"` //index 2
+	Genus    string        `json:"genus"`  //index 1
 	AllNames SnakeSynonyms `json:"allNames"`
 }
 type SnakeSynonyms struct {
-	Name string `json:"binomial"`
+	Name     string   `json:"binomial"`
 	Synonyms []string `json:"synonyms"` //12 > ? (must test for  number of columns  with text(or use no_syn at index 9)
 }
 
@@ -28,7 +27,7 @@ func handleErr(err error) {
 	}
 }
 
-func loadCSV(csvLoc string) () {
+func loadCSV(csvLoc string) {
 	f, err := os.Open(csvLoc)
 	handleErr(err)
 	defer f.Close()
@@ -43,8 +42,8 @@ func loadCSV(csvLoc string) () {
 			continue
 		}
 		binomial := strings.TrimSpace(line[3])
-		family := strings.Trim(line[2], " ")
-		genus := strings.Trim(line[1], " ")
+		family := strings.Trim(line[1], " ")
+		genus := strings.Trim(line[2], " ")
 		numSynonymsStr := line[9]
 		if len(numSynonymsStr) > 0 {
 			numSynonyms, err := strconv.Atoi(numSynonymsStr)
@@ -55,21 +54,24 @@ func loadCSV(csvLoc string) () {
 				for i := 12; i < 12+numSynonyms; i++ {
 					syn := strings.TrimSpace(line[i])
 
-					if strings.Contains(syn, "?") == false  && len(syn) != 0 && strings.Compare(syn, binomial) != 0 && strings.Compare(syn, "NA") != 0 {
+					if strings.Contains(syn, "?") == false && len(syn) != 0 && strings.Compare(syn, binomial) != 0 && strings.Compare(syn, "NA") != 0 {
 						fmt.Println(len(syn), syn)
+						syn = strings.Replace(syn, "(", "", -1)
+						syn = strings.Replace(syn, "\ufffd", "ü", -1)
 						synonyms[synLength] = syn
 						synLength++
 					}
 				}
-				a := SnakeSynonyms{Name:binomial, Synonyms:synonyms}
-				s := Snake{Binomial: binomial, Family: family, Genus: genus, AllNames: a}
+				synonyms = append([]string(nil), synonyms[:synLength]...)
+				a := SnakeSynonyms{Name: binomial, Synonyms: synonyms}
+				s := Snake{Family: family, Genus: genus, AllNames: a}
 				snakes[index] = s
 			}
 		}
 		//fmt.Println(binomial, family, genus, numSynonyms)
 	}
-	snakeJson, _ := json.MarshalIndent(snakes, "", " ")
-	err = ioutil.WriteFile("/Users/encima/dev/go/uzh_tools/snapp/data/snakes.json", snakeJson, 0644)
+	snakeJSON, _ := json.MarshalIndent(snakes, "", " ")
+	err = ioutil.WriteFile("./data/snakes.json", snakeJSON, 0644)
 	handleErr(err)
 }
 
@@ -79,5 +81,5 @@ func parseSnakes(line []string) (s Snake) {
 }
 
 func main() {
-	loadCSV("/Users/encima/dev/go/uzh_tools/snapp/data/trd18-splitted-synonyms-jagged-families.csv")
+	loadCSV("./data/trd18-splitted-synonyms-jagged-families.csv")
 }
