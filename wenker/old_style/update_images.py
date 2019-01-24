a1280143c81f9fe16ca6d56072e5eb2aed57fd86
Christopher Gwilliams <encima@gmail.com>
2019-01-24 13:46:24 +0100

    added update handling for json columns i wenker

--- wenker/old_style/update_images.py
+++ wenker/old_style/update_images.py
@@ -1,28 +1,33 @@
 import psycopg2
+import config
 
 
 IMG_NAMES = '/Users/encima/.CMVolumes/Nextcloud/Documents/uzh/wenker_CH_kurrent.txt'
 DB_STRING = 'postgresql://pybossa:tester@wenker.citizenscience.ch/cs'
 
-UPDATE_STRING = "UPDATE tasks SET info = info || {'old_style': 1} WHERE id = '{}'"
+UPDATE_STRING = "UPDATE tasks SET info = info || '{{\"old_style\": 1}}' WHERE id = '{}';"
 SELECT_STRING = "SELECT * FROM tasks WHERE info ->> 'path'::text like '%{}%'::text"
 
 class OldStyleUpdate:
 
     def __init__(self):
-        self.conn = psycopg2.connect(host="wenker.citizenscience.ch",database="cs", user="pybossa", password="tester")
+        self.conn = psycopg2.connect(host=config.DB_HOST,database=config.DB_NAME, user=config.DB_USER, password=config.DB_PWD)
 
     def process(self):
         with open(IMG_NAMES) as images:
             for image in images:
-                # print(image)
                 img = image.replace('.jpg', '').rstrip()
                 sel = SELECT_STRING.format(img)
                 print(sel)
                 cursor = self.conn.cursor()
                 cursor.execute(sel)
                 res = cursor.fetchone()
-                print(res)
+                if res is not None:
+                  print(res[0])
+                  print(UPDATE_STRING.format(res[0]))
+                  cursor.execute(UPDATE_STRING.format(res[0]))
+                else:
+                  print("{} not updated".format(img))
 
 
 
