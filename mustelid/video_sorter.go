0e84e8fa0ab2325eb21d88162b4f06eeca7cf886
encima <encima@gmail.com>
2019-08-12 15:28:31 +0200

    mustelid video time tracking

---
+++ mustelid/video_sorter.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"path/filepath"
+)
+
+func walkDir(path string, info os.FileInfo, err error) error {
+	if err != nil {
+		fmt.Printf("prevent panic by handling failure accessing a path %q: %v \n", path, err)
+		return err
+	}
+	if !info.IsDir() && info.Name() != "sample_data" {
+		fmt.Printf("%s, %s, %dMB", info.Name(), info.ModTime().String(), (info.Size()/1024)/1024)
+		fmt.Printf(" visited file or dir: %q \n", path)
+
+	}
+
+	return nil
+}
+
+func main() {
+
+	srcDir := "./sample_data"
+	err := filepath.Walk(srcDir, walkDir)
+	if err != nil {
+		fmt.Printf("error walking the path %q: %v\n", srcDir, err)
+		return
+	}
+}
